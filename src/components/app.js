6baafc9aa8fb53490bde2d7f92650d31c7b516ae
andypagdin <andyjpagdin@gmail.com>
2019-12-11 15:28:12 +0000

    Create app component

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Home from './home'
+import About from './about'
+import PageNotFound from './page-not-found'
+
+export default function app () {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route path="*" component={PageNotFound} />
+      </Switch>
+    </Router>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import Home from './components/home'
-import About from './components/about'
-import PageNotFound from './components/page-not-found'
+import App from './components/app'
 
-ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route path="*" component={PageNotFound} />
-    </Switch>
-  </Router>,
-  document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById('app'))
